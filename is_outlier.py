707bdbd3b0c399c44f1025267300499a9b4c22ac
Naïa <naia.perinelle@laposte.net>
2024-02-28 09:01:45 +0100

    import numpy for is_outlier

--- is_outlier.py
+++ is_outlier.py
@@ -1,6 +1,8 @@
 """Single-Cell Analysis in Python.
 https://www.sc-best-practices.org/preprocessing_visualization/quality_control.html
 """
+import numpy as np
+
 # def is-outlier sc best practices
 # function that takes a metric, i.e. a column in .obs and the number of MADs (nmad) that is still permissive within the filtering strategy
 def is_outlier(adata, metric: str, nmads: int):
