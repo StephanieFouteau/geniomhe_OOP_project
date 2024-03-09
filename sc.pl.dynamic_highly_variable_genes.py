d2a7e84670f17735a89bce735d54e6316c7cbef1
stephanievincent <svincent@genoscope.cns.fr>
2024-03-09 18:37:55 +0100

    dynamic plot for scanpy.pp.highly_variable_genes function result

---
+++ sc.pl.dynamic_highly_variable_genes.py
@@ -0,0 +1,17 @@
+import scanpy as sc
+import plotly as py
+import plotly.express as px
+
+
+    """Dynamically plot dispersions or normalized variance versus means for genes. 
+
+    adata
+        Result of :func:`~scanpy.pp.highly_variable_genes`.
+   
+    """
+
+    def dynamic_plot_highly_variable_genes(adata):
+    
+    fig = px.scatter(adata.var, x="means", y="dispersions", color="highly_variable", hover_data=['gene_ids'])
+    fig.show()
+    return fig
