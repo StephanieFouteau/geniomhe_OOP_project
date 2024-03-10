32625317c820d3bed5cc6caf7953f1af52d70ff1
Naïa <naia.perinelle@laposte.net>
2024-03-10 21:10:50 +0100

    correction of indentation

--- sc.pl.dynamic_highly_variable_genes.py
+++ sc.pl.dynamic_highly_variable_genes.py
@@ -3,14 +3,14 @@ import plotly as py
 import plotly.express as px
 
 
-    """Dynamically plot dispersions versus means for genes. 
+"""Dynamically plot dispersions versus means for genes. 
 
-    adata
-        Result of :func:`~scanpy.pp.highly_variable_genes`.
+adata
+    Result of :func:`~scanpy.pp.highly_variable_genes`.
    
-    """
+"""
 
-    def dynamic_plot_highly_variable_genes(adata):
+def dynamic_plot_highly_variable_genes(adata):
     
     fig = px.scatter(adata.var, x="means", y="dispersions", color="highly_variable", hover_data=['gene_ids'])
     fig.show()
